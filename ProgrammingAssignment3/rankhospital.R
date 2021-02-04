0fe898f9dcbce04ce9cca77a0d81522ecfee72f3
Trisotto <longaraitrisotto.flt@gmail.com>
2021-02-03 21:06:18 -0300

    Solution to rank hospitals by outcome in a state.

---
+++ ProgrammingAssignment3/rankhospital.R
@@ -0,0 +1,41 @@
+# The function reads the outcome-of-care-measures.csv file and returns a character vector with the name
+# of the hospital that has the ranking specified by the num argument.
+rankhospital <- function(state, outcome, num = 'best') {
+        # Read data
+        data <- read.csv("DataFiles/outcome-of-care-measures.csv", colClasses = "character")
+        
+        # State and Outcome Validation
+        if (!state %in% unique(data$State)) {
+                stop('invalid state')
+        } else if (!outcome %in% c("heart attack","heart failure","pneumonia")) {
+                stop('invalid outcome')
+        }
+        
+        # Filters by state
+        data <- data[data$State == state,]
+        
+        # Removes not needed columns
+        data <- data[c(2,11,17,23)]
+        
+        # Renames columns
+        names(data) <- c("hospital","heart attack","heart failure","pneumonia")
+        
+        # Convert outcome columns to numeric
+        data[,2:4] <- apply(data[,2:4],2,function(x) suppressWarnings(as.numeric(x)))
+        
+        # Remove NA values
+        data <- data[complete.cases(data),]
+        
+        # Sort data in ascending order
+        data <- data[order(data[outcome],data$hospital),]
+        
+        ## Return hospital name in that state with the given rank 30-day death rate
+        if (num == "best"){
+                return(head(data$hospital,1))
+        } else if (num == "worst"){
+                return(tail(data$hospital,1))
+        } else {
+                return(data[num,"hospital"])
+        }
+        
+}
